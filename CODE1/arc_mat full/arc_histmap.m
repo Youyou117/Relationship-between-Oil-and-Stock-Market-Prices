3f52d8c842d43fb6eb869be89c199ee40ac0bc01
YOuyou117 <xiaoyouzhou117@gmail.com>
2020-03-07 15:40:29 -0500

    add report file

--- CODE1/arc_mat full/arc_histmap.m
+++
@@ -1,8 +0,0 @@
-function arc_histmap(pltvariables,results,options)
-    data = am_DataSource(pltvariables,results,options);
-    hist = am_Histogram(data);
-    map = am_BaseMap(data);
-    link = am_Linkage;
-    link.addMap(map);
-    link.addGraph(hist);
-end
--- CODE1/arc_mat full/arc_moranplot.m
+++
@@ -1,8 +0,0 @@
-function arc_moranplot(variable,W,results,options)
-    data = am_DataSource(variable,results,options);
-    moran = am_Moranplot(data);
-    map = am_BaseMap(data);
-    link = am_Linkage;
-    link.addMap(map);
-    link.addGraph(moran);
-end
